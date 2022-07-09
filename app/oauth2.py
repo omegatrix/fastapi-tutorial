6d9a687fb59f5b92b15299a27d07f95eb584fe0e
Arnold Anthonypillai <arnoldbronson16@gmail.com>
2022-07-09 18:45:49 +0100

    Add env vars

--- app/oauth2.py
+++ app/oauth2.py
@@ -8,28 +8,29 @@ from sqlalchemy.orm import Session
 from .database import get_db
 from .models import User
 from .schemas import TokenData
-
-SECRET_KEY = "fff4554cd2eaa467a3243a576b0c03ab3df82a5f968d83bd20f2873b76b0b572"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from .settings import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="authenticate")
 
 
 def generate_access_token(data: dict):
     to_encode = data.copy()
-    to_expire = f"{datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)}"
+    to_expire = f"{datetime.utcnow() + timedelta(minutes=settings.jwt_access_token_expiry_minutes)}"
 
     to_encode.update({"expire": to_expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.jwt_secret, algorithm=settings.jwt_algorithm
+    )
 
     return encoded_jwt
 
 
 def verify_access_token(token: str, credentials_exception: Exception) -> TokenData:
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(
+            token, settings.jwt_secret, algorithm=settings.jwt_algorithm
+        )
         user_id: str = payload.get("user_id")
 
         if not user_id:
