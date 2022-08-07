015ebb7663de2e55aa308547341ec24cb43230c5
Arnold Anthonypillai <arnoldbronson16@gmail.com>
2022-08-07 12:01:23 +0100

    Add requirements

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
-from . import models
-from .database import engine
 from .routers import auth, post, user, vote
-
-# models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
